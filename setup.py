f41c28d3535129a96007d86dccb9f50b60fa8ab4
josrirser <josrirser@gmail.com>
2020-07-17 13:47:04 +0800

    'setup.py'

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+# readme.md = github readme.md, 這裡可接受markdown寫法
+# 如果沒有的話，需要自己打出介紹此專案的檔案，再讓程式知道
+
+with open("Discription.md", "r",encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="VerdictFormat", # 
+    version='1.0',
+    author="Josiriser",
+    author_email="josiriser@gmail.com",
+    description="To format verdict",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Josiriser/VerdictFormat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
