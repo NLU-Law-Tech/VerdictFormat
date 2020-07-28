0a0ba56f5f636117aacaa3c3a4e1f4129efb70da
josrirser <josrirser@gmail.com>
2020-07-28 09:39:56 +0800

    'Update'

--- VerdictFormat/VerdictFormat.py
+++ VerdictFormat/VerdictFormat.py
@@ -1,6 +1,5 @@
 import json
 import re
-
 import requests
 
 def Formal_to_Test(Formal_format_path,output_path):
@@ -83,20 +82,30 @@ def Labeled_to_Test(Labeled_data):
         output_dict.clear()
     return output_dict_list
 
-def Multilaws_to_Normalize(Multilaws,break_line="\r\n"):
+def Multilaws_to_Normalize(CJ_text,Match_laws_list,Multilaws_dict_list,Break_line="\r\n"):
     Normalized_laws_list=[]
-    # 資料清洗
-    clean_Multilaws=re.sub(break_line,"",strip_blank(Multilaws))
-    # 先找出法律名稱
-    laws_name=get_laws_name(clean_Multilaws)
-    # 頓號隔開第幾條
-    temp_list=clean_Multilaws.split("、")
-    for i in range(len(temp_list)):
-        #第一項通常會寫完整(包含法律名稱跟第幾條)
-        if i==0:
-            Normalized_laws_list.append(temp_list[i])
-        else:
-            Normalized_laws_list.append(laws_name+temp_list[i])
+    for Multilaws_dict in Multilaws_dict_list:
+        content=Multilaws_dict["content"]
+        start=Multilaws_dict["start"]
+         # 先找出法律名稱
+        laws_name=get_laws_name(content,start,CJ_text,Match_laws_list)
+        # 資料清洗
+        clean_Multilaws=re.sub(Break_line,"",strip_blank(content))
+        # 取出第幾條第幾項第幾款,act則用 laws_name代替
+        act,article,paragraph,subparagraph=extract_laws_spa(clean_Multilaws)
+        
+        # 因為轉出來是數字，所以要再轉回字串
+        s_article=""
+        s_paragraph=""
+        s_subparagraph=""
+        if article!="":
+            s_article="第"+str(article)+"條"
+        if paragraph!="":
+            s_paragraph="第"+str(paragraph)+"項"
+        if subparagraph!="":
+            s_subparagraph="第"+str(subparagraph)+"款"
+        Normalized_laws_list.append(laws_name+s_article+s_paragraph+s_subparagraph)
+
     return Normalized_laws_list
 
 def get_posistions_dict(job_location_list,job_title_list):
@@ -252,14 +261,29 @@ def strip_blank(dirty_str):
     clean_str=re.sub(r"\s+","",dirty_str)
     return clean_str
 
-def get_laws_name(laws):
-    result=laws
-    # 找第一個 "第"  這個字前面的法律名稱
-    laws_name_position=re.search("第",laws)
-    if laws_name_position!= None:
-        result=laws[:laws_name_position.end()-1]
-
-    return result
+def get_laws_name(laws,origin_start,CJ_text,Match_laws_list):
+    laws_name_dict_list=[]
+    laws_name_dict={}
+    for law in Match_laws_list:
+        # 先找該法律名稱是否有在CJ_text
+        if re.search(law,CJ_text)==None:
+            continue
+        else:
+            # 如果有則找出所有位置
+            all_match_positions=re.finditer(law,CJ_text)
+            if len(laws_name_dict)==0:
+                distance=99999999999
+            else:
+                distance=laws_name_dict["distance"]
+            for match_position in all_match_positions:  
+                # 計算距離多遠  並且法律名稱要在 origin_start 前面
+                temp_distance= origin_start-match_position.start()
+                # 要找跟laws最接近的位置
+                if temp_distance < distance and temp_distance >=0 :
+                    distance=temp_distance
+                    laws_name_dict["law"]=law
+                    laws_name_dict["distance"]=distance
+    return laws_name_dict["law"]
 
 if __name__ == "__main__":
     # Formal_file_path="C:/Yao/ITRI/API (1)/output_v2.json"
@@ -279,7 +303,37 @@ if __name__ == "__main__":
     # labeled_data=response_data.json()
     # print(Labeled_to_Test(labeled_data))
   
-    # Multilaws="貪汙治罪條\r\n例第五條、第\r\n八條"
-    Multilaws="貪汙治罪條\\r\\n例第五條、第\r\n八條\\r\\n第五項第六款、第 \r\n 十 \\r\\n 條第六項第七款"
-    Normalized_laws_list=Multilaws_to_Normalize(Multilaws,"\\\\r\\\\n")
+    Multilaws_dict_list=[
+        {"start": 2933, "content": "毒品危害防制條例第11條"},
+        {"start": 2946, "content": "第13條"},
+        {"start": 2951, "content": "第15\r\n條"},
+        {"start": 3315, "content": "貪污治罪條例第11條"},
+        {"start": 3326, "content": "第133333條第8項\r\n第6款"},
+    ]
+    Match_laws_list=['中華民國刑法', '陸海空軍刑法', '國家機密保護法', '國家情報工作法', 
+                    '國家安全法', '洗錢防制法', '臺灣地區與大陸地區人民關係條例', '貿易法', 
+                    '組織犯罪防制條例', '人口販運防制法', '社會秩序維護法', '戰略性高科技貨品輸出入管理辦法', 
+                    '山坡地保育利用條例', '公司法', '公民投票法', '公職人員選舉罷免法', 
+                    '水土保持法', '水污染防治法', '水利法', '兒童及少年性交易防制條例', 
+                    '空氣污染防制法', '金融控股公司法', '律師法', '政府採購法', '毒品危害防制條例',
+                    '區域計畫法', '國有財產法', '票券金融管理法', '貪污治罪條例', 
+                    '都市計畫法', '期貨交易法', '森林法', '稅捐稽徵法', '農田水利會組織通則',
+                    '農會法', '農業金融法', '槍砲彈藥刀械管制條例', '漁會法', '銀行法',
+                    '廢棄物清理法', '總統副總統選舉罷免法', '懲治走私條例', '藥事法', '證券交易法', 
+                    '資恐防制法', '畜牧法', '破產法', '商標法', '商業登記法', '光碟管理條例',
+                    '個人資料保護法', '健康食品管理法', '妨害國幣懲治條例', '通訊保障及監察法',
+                    '化粧品衛生管理條例', '金融資產證券化條例', '食品安全衛生管理法',
+                    '動物傳染病防治條例', '多層次傳銷管理法', '商業會計法', '信託業法',
+                    '電信法', '動物用藥品管理法', '消費者債務清理條例', '專利師法',
+                    '傳染病防治法', '嚴重特殊傳染性肺炎防治及紓困振興特別條例',
+                    '農藥管理法', '飼料管理法', '管理外匯條例', '野生動物保育法',
+                    '植物防疫檢疫法', '遺產及贈與稅法', '電子支付機構管理條例', 
+                    '電子票證發行管理條例', '營業秘密法', '信用合作社法', '菸酒管理法', 
+                    '保險法', '證券投資信託及顧問法', '證券投資人及期貨交易人保護法']
+    
+    file_path="C:/Yao/ITRI/Work/Project/testing_data/5d30daa0cbd1c48dc9762e8f_2.json"
+    with open(file_path,'r',encoding='utf-8') as f:
+        full_text=json.load(f)
+    CJ_text=full_text["judgement"]
+    Normalized_laws_list=Multilaws_to_Normalize(CJ_text,Match_laws_list,Multilaws_dict_list)
     print(Normalized_laws_list)
