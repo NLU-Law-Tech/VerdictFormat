702c5c83bdbcfe4ebb2de8b4f9c7f4a3c739df3d
josrirser <josrirser@gmail.com>
2020-08-05 16:37:12 +0800

    fix chinese number bug

--- VerdictFormat/VerdictFormat.py
+++ VerdictFormat/VerdictFormat.py
@@ -187,32 +187,63 @@ def get_laws_dict(laws_list):
 
 def extract_laws_spa(law):
     # 取出法條的款項條
-    regex_article="第\d*條"
-    regex_paragraph="第\d*項"
-    regex_subparagraph="第\d*款"
-    # 假設都沒找到就回傳law
-    act=law
-    article=""
-    paragraph=""
-    subparagraph=""
-    # 找第幾條，有找到再往下找第幾項
-    article_position=re.search(regex_article,law)
-    if article_position != None:
-        # 取出act(法條名稱，不包含條項款)
-        act=law[:article_position.start()]
-        article_text=law[article_position.start():article_position.end()]
-        # 取出數字
-        article=get_laws_number(article_text)
-        # 找第幾項，有找到再往下找第幾款
-        paragraph_position=re.search(regex_paragraph,law)
-        if paragraph_position != None:
-            paragraph_text=law[paragraph_position.start():paragraph_position.end()]
-            paragraph=get_laws_number(paragraph_text)
-            subparagraph_position=re.search(regex_subparagraph,law)
-            if subparagraph_position != None:
-                subparagraph_text=law[subparagraph_position.start():subparagraph_position.end()]
-                subparagraph=get_laws_number(subparagraph_text)
+    if re.search("\d",law) !=None:
+        # 用阿拉伯數字寫數字
+        regex_article="第\d*條"
+        regex_paragraph="第\d*項"
+        regex_subparagraph="第\d*款"
+        mode =1
+    else:
+        regex_article="第.*條"
+        regex_paragraph="第.*項"
+        regex_subparagraph="第.*款"
+        mode =2
 
+    if mode ==1:
+        # 假設都沒找到就回傳law
+        act=law
+        article=""
+        paragraph=""
+        subparagraph=""
+        # 找第幾條，有找到再往下找第幾項
+        article_position=re.search(regex_article,law)
+        if article_position != None:
+            # 取出act(法條名稱，不包含條項款)
+            act=law[:article_position.start()]
+            article_text=law[article_position.start():article_position.end()]
+            # 取出數字
+            article=get_laws_number(article_text)
+            # 找第幾項，有找到再往下找第幾款
+            paragraph_position=re.search(regex_paragraph,law)
+            if paragraph_position != None:
+                paragraph_text=law[paragraph_position.start():paragraph_position.end()]
+                paragraph=get_laws_number(paragraph_text)
+                subparagraph_position=re.search(regex_subparagraph,law)
+                if subparagraph_position != None:
+                    subparagraph_text=law[subparagraph_position.start():subparagraph_position.end()]
+                    subparagraph=get_laws_number(subparagraph_text)
+    else:
+        act=law
+        article=""
+        paragraph=""
+        subparagraph=""
+        # 找第幾條，有找到再往下找第幾項
+        article_position=re.search(regex_article,law)
+        if article_position != None:
+            # 取出act(法條名稱，不包含條項款)
+            act=law[:article_position.start()]
+            article_text=law[article_position.start():article_position.end()]
+            # 取出數字
+            article=article_text[1:len(article_text)-1]
+             # 找第幾項，有找到再往下找第幾款
+            paragraph_position=re.search(regex_paragraph,law)
+            if paragraph_position != None:
+                paragraph_text=law[paragraph_position.start():paragraph_position.end()]
+                paragraph=paragraph_text[1:len(paragraph_text)-1]
+                subparagraph_position=re.search(regex_subparagraph,law)
+                if subparagraph_position != None:
+                    subparagraph_text=law[subparagraph_position.start():subparagraph_position.end()]
+                    subparagraph=subparagraph_text[1:len(subparagraph_text)-1]
     return act,article,paragraph,subparagraph
 
 def get_laws_number(laws_with_number):
@@ -317,7 +348,8 @@ if __name__ == "__main__":
     #     {"start": 3315, "content": "貪污治罪條例第11條"},
     #     {"start": 3326, "content": "第133333條第8項\r\n第6款"},
     # ]
-    Multilaws_dict_list=[{'end': 37334, 'start': 37320, 'content': '\r\n中華民國刑法第158條'}, {'end': 37410, 'start': 37398, 'content': '中華民國刑法第216條'}, {'end': 37480, 'start': 37468, 'content': '中華民國刑法第211條'}, {'end': 37538, 'start': 37526, 'content': '中華民國刑法第212條'}, {'end': 37637, 'start': 37625, 'content': '中華民國刑法第339條'}]
+    Multilaws_dict_list={"end": 6738, "start": 6728, "content": "刑法第三百三十九條之四"}, {"end": 6889, "start": 6882, "content": "洗錢防制法第二條"}, {"end": 7037, "start": 7029, "content": "洗錢防制法第十四條"}
+   
     Match_laws_list=['中華民國刑法', '陸海空軍刑法', '國家機密保護法', '國家情報工作法', 
                     '國家安全法', '洗錢防制法', '臺灣地區與大陸地區人民關係條例', '貿易法', 
                     '組織犯罪防制條例', '人口販運防制法', '社會秩序維護法', '戰略性高科技貨品輸出入管理辦法', 
@@ -337,12 +369,14 @@ if __name__ == "__main__":
                     '農藥管理法', '飼料管理法', '管理外匯條例', '野生動物保育法',
                     '植物防疫檢疫法', '遺產及贈與稅法', '電子支付機構管理條例', 
                     '電子票證發行管理條例', '營業秘密法', '信用合作社法', '菸酒管理法', 
-                    '保險法', '證券投資信託及顧問法', '證券投資人及期貨交易人保護法']
+                    '保險法', '證券投資信託及顧問法', '證券投資人及期貨交易人保護法','刑法']
     
-    file_path="C:/Yao/ITRI/Work/Project/testing_data/5d30db00cbd1c48dc9787708.txt"
+    file_path="C:/Yao/ITRI/Work/Project/testing_data/5d30db5bcbd1c48dc9794cb0.txt"
     with open(file_path,'r',encoding='utf-8') as f:
-        full_text=json.load(f)
-    CJ_text=full_text["judgement"]
+        # full_text=json.load(f)
+        full_text=f.read()
+    # CJ_text=full_text["judgement"]
+    CJ_text=full_text
     # test_path="C:/Yao/ITRI/Work/Project/testing_data/5d30db00cbd1c48dc9787708_1.txt"
     # f = open(test_path,'w',encoding='utf-8')
     # f.writelines(full_text["judgement"])
